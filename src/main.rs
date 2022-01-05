daa74fe24f5f88f2aea7d122669aca24f6fe009d
ccmlm <fanhui.x@gmail.com>
2022-01-06 00:13:50 +0800

    parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use futures::executor::block_on;
 use ruc::*;
 use secp256k1::SecretKey;
@@ -16,8 +17,9 @@ fn main() {
 }
 
 fn run() -> Result<()> {
-    let cmdline = parse_cmdline().c(d!())?;
-    let url = if cmdline.use_testnet {
+    let args = Args::parse();
+
+    let url = if args.testnet {
         BSC_TESTNET
     } else {
         BSC_MAINNET
@@ -26,61 +28,60 @@ fn run() -> Result<()> {
     let transport = web3::transports::Http::new(url).c(d!())?;
     let web3 = web3::Web3::new(transport);
 
-    let prvk = SecretKey::from_str(&pnk!(fs::read_to_string(cmdline.privkey_path))).c(d!())?;
+    let prvk = SecretKey::from_str(&pnk!(fs::read_to_string(args.privkey_path))).c(d!())?;
+    let contents = fs::read_to_string(args.entries_path).c(d!())?;
 
-    for l in fs::read_to_string(cmdline.entries_path).c(d!())?.lines() {
-        let res = l.split(',').collect::<Vec<_>>();
-
-        if 2 != res.len() {
+    let mut entries = vec![];
+    for l in contents.lines() {
+        let en = l.split(',').collect::<Vec<_>>();
+        if 2 != en.len() {
             return Err(eg!(format!("Invalid entry: {}", l)));
         }
-
-        let receiver = res[0];
-        if !res[0].starts_with("0x") {
+        if !en[0].starts_with("0x") || 34 != en[0].len() {
             return Err(eg!(format!("Invalid entry: {}", l)));
         }
-
-        let amount = res[1]
+        let receiver = en[0];
+        let amount = en[1]
             .parse::<f64>()
-            .c(d!())
-            .or_else(|e| res[1].parse::<u128>().map(|am| am as f64).c(d!(e)))?;
+            .or_else(|_| en[1].parse::<u128>().map(|am| am as f64))
+            .c(d!("Invalid amount"))?;
+        entries.push((receiver, amount));
+    }
 
-        let am = amount as u128 * 10u128.pow(18);
-
+    for (receiver, amount) in entries.into_iter() {
+        let am = (amount * (10u128.pow(18) as f64)) as u128;
         let tx = TransactionParameters {
-            to: Some(pnk!(Address::from_str(receiver))),
-            value: pnk!(U256::from_dec_str(&am.to_string())),
+            to: Some(Address::from_str(receiver).c(d!())?),
+            value: U256::from_dec_str(&am.to_string()).c(d!())?,
             ..Default::default()
         };
-
         let signed =
             block_on(async { web3.accounts().sign_transaction(tx, &prvk).await }).c(d!())?;
-
         let result = block_on(async {
             web3.eth()
                 .send_raw_transaction(signed.raw_transaction)
                 .await
         })
         .c(d!())?;
-
         println!(
             "Tx succeeded with hash: {}, to: {}, amount: {}",
-            result, receiver, am
+            result, receiver, amount
         );
     }
 
     Ok(())
 }
 
-fn parse_cmdline() -> Result<CmdLine> {
-    todo!()
-}
-
-struct CmdLine {
-    use_testnet: bool,
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+    #[clap(short, long)]
+    testnet: bool,
     // A file contains how much to transfer:
     // - 0xAAAAAAA...AAAAAAAAAA <amount>
     // - ...
+    #[clap(short, long)]
     entries_path: String,
+    #[clap(short, long)]
     privkey_path: String,
 }
